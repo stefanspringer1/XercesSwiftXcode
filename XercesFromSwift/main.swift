b0b090ad5b0764dc67734afa66f6b5375e378743
Stefan Springer <stefan.springer@stefanspringer.com>
2021-04-28 12:55:31 +0200

    get path to XML file as argument

--- XercesFromSwift/main.swift
+++ XercesFromSwift/main.swift
@@ -15,7 +15,12 @@ class MyXMLParseConsumer: XMLParseConsumer {
     
 }
 
-let parseConsumer = MyXMLParseConsumer()
-let parser = XercesParser(withParseConsumer: parseConsumer)
-let returnCode = parser.parse(fromPath: "/Users/stefan/Projekte/SwiftXML/test.xml")
-print("return code: \(returnCode)")
+if CommandLine.arguments.count != 2 {
+    print("ERROR: need exactly one argument = path XML file to parse")
+}
+else {
+    let parseConsumer = MyXMLParseConsumer()
+    let parser = XercesParser(withParseConsumer: parseConsumer)
+    let returnCode = parser.parse(fromPath: CommandLine.arguments[1])
+    print("return code: \(returnCode)")
+}
